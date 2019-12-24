91d43919a197909a570590e86babe7e640dbfa1f
Mayank <mayank.gupta@iongroup.com>
2019-12-24 11:16:57 +0530

    Minor Code Refractoring

--- src/app/container/appComponent/app.component.ts
+++ src/app/container/appComponent/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
+import ApplicationDataService from "../../services/application-data.service";
 import RepoDetails from "../../entities/RepoDetails";
 import CommitDetails from "../../entities/CommitDetails";
-import ApplicationDataService from "../../services/application-data.service";
 
 @Component({
   selector: 'app-root',
@@ -15,27 +16,11 @@ export default class AppComponent implements OnInit {
 
   ngOnInit() {
     this.appData.getRepositoryData().subscribe((res)=>{
-      this.repositoryDetails = new RepoDetails()
-      this.repositoryDetails.avatarUrl = res['organization']['avatar_url'];
-      this.repositoryDetails.ownerLogin = res['owner']['login'];
-      this.repositoryDetails.repositoryName = res['name'];
-      this.repositoryDetails.homePage = res["homepage"];
-      this.repositoryDetails.repoSize = res["size"];
-      this.repositoryDetails.subscribersCount = res["subscribers_count"];
-      this.repositoryDetails.defaultBranch = res["default_branch"];
-      this.repositoryDetails.openIssueCount = res["open_issues_count"];
-      this.repositoryDetails.language = res["language"];
+      this.repositoryDetails = this.appData.repositoryToObjectMapper(res);
     });
 
     this.appData.getCommitData().subscribe((commitList: any)=>{
-      commitList.forEach(commitData => {
-        let singleCommit: CommitDetails = new CommitDetails();
-        singleCommit.author = commitData.commit.author.name;
-        singleCommit.avatarUrl = commitData.author.avatar_url;
-        singleCommit.message = commitData.commit.message
-        singleCommit.email = commitData.commit.author.email
-        this.commitList.push(singleCommit);
-      });
+      this.commitList = this.appData.commitToListMapper(commitList);
     });
   }
 }
--- src/app/services/application-data.service.ts
+++ src/app/services/application-data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import RepoDetails from "../entities/RepoDetails";
+import CommitDetails from "../entities/CommitDetails";
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +17,33 @@ export default class ApplicationDataService {
 
   getCommitData(): Observable<any> {
     return this._http.get("https://api.github.com/repos/angular/angular/commits")
+  }
+
+  repositoryToObjectMapper(res: any): RepoDetails {
+    var repositoryDetails = new RepoDetails()
+    repositoryDetails.avatarUrl = res['organization']['avatar_url'];
+    repositoryDetails.ownerLogin = res['owner']['login'];
+    repositoryDetails.repositoryName = res['name'];
+    repositoryDetails.homePage = res["homepage"];
+    repositoryDetails.repoSize = res["size"];
+    repositoryDetails.subscribersCount = res["subscribers_count"];
+    repositoryDetails.defaultBranch = res["default_branch"];
+    repositoryDetails.openIssueCount = res["open_issues_count"];
+    repositoryDetails.language = res["language"];
+    return repositoryDetails;
+  }
+
+  commitToListMapper(commitList: any): CommitDetails[] {
+    let commitReturnList: CommitDetails[] = [];
+    commitList.forEach(commitData => {
+      let singleCommit: CommitDetails = new CommitDetails();
+      singleCommit.author = commitData.commit.author.name;
+      singleCommit.avatarUrl = commitData.author.avatar_url;
+      singleCommit.message = commitData.commit.message
+      singleCommit.email = commitData.commit.author.email
+      commitReturnList.push(singleCommit);
+    });
+
+    return commitReturnList;
   }
 }
